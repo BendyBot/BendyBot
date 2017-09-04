736bbcfa0b0f455374d9dfa4aa45008dece5b8a8
Saad Meliti <30853136+Saadmeliti@users.noreply.github.com>
2017-09-04 07:41:39 -0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = 'MzM4MjQ2OTI0Mjc4ODI0OTcw.DFdPjg.b_5ewe09xr35QbIUKSMeccbCXTU';
+const prefix = "^"
+bot.on('ready', () => {
+  console.log('BendyBot Ready');
+});
+bot.on('message', message => {
+if (message.content === 'Hello, BendyBot!') {
+message.channel.sendMessage('Secret unlocked: Hello Bendy');
+  }
+});
+bot.login(token);
+bot.on('message', message => {
+if (message.content === 'Gimme all the bacon soup!') {
+message.channel.sendMessage('Secret unlocked: Canadian Bacon');
+  }
+});
+bot.on('message', message => {
+if (message.content === 'Can i be mod bendy?') {
+message.channel.sendMessage('No, you are not inky enough.');
+  }
+});
+bot.on('message', message => {
+if (message.content === 'chapter three is coming out soon') {
+message.channel.sendMessage('👹 *yes indeed my friend...* 👹');
+  }
+});
