d16408effb8d9c15507b6951250c188c2035693c
Saad Meliti <30853136+Saadmeliti@users.noreply.github.com>
2017-09-05 03:39:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'token here';
+const token = 'YOUR BOTS DISCORD TOKEN';
 const prefix = "^"
 bot.on('ready', () => {
   console.log('BendyBot Ready');
@@ -24,5 +24,20 @@ message.channel.sendMessage('No, you are not inky enough.');
 bot.on('message', message => {
 if (message.content === 'chapter three is coming out soon') {
 message.channel.sendMessage('👹 *yes indeed my friend...* 👹');
+  }
+});
+bot.on('message', message => {
+  if (message.content === 'what is my avatar bendy?') {
+    message.channel.sendMessage(message.author.avatarURL);
+  }
+});
+bot.on('message', message => {
+  if (message.content === 'give me an inky rating') {
+    message.channel.sendMessage('i would rate you ' + Math.floor(Math.random() * 100));
+  }
+});
+bot.on('message', message => {
+if (message.content === 'when is chapter 3 coming') {
+message.channel.sendMessage('👹*This month...*👹');
   }
 });
