522d9b92cc99274fbf508a3b92cddb54cd1e797e
Saad Meliti <30853136+Saadmeliti@users.noreply.github.com>
2017-09-10 07:13:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'BOT TOKEN HERE';
+const token = 'MzM4MjQ2OTI0Mjc4ODI0OTcw.DI79_Q.eMSZcjtCT0qMwRmr7ZFRBk8sIRU';
 const prefix = "^"
 bot.on('ready', () => {
   console.log('BendyBot Ready');
@@ -27,7 +27,7 @@ message.channel.sendMessage('No, you are not inky enough.');
 });
 bot.on('message', message => {
 if (message.content === 'chapter three is coming out soon') {
-message.channel.sendMessage('👹 *yes indeed my friend...* 👹');
+message.channel.sendMessage('👹*yes indeed my friend...*👹');
   }
 });
 bot.on('message', message => {
@@ -45,13 +45,24 @@ if (message.content === 'when is chapter 3 coming') {
 message.channel.sendMessage('👹*This month...*👹');
   }
 });
+const embed = new Discord.RichEmbed()
+.setTitle('BendyBot help')
+.setAuthor('BendyBot', 'https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
+.setColor(0x00AE86)
+.setDescription('all the commands for BendyBot')
+.setFooter('thats all the commands for now', 'https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
+.setThumbnail('https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
+.setTimestamp()
+.setURL('https://github.com/BendyBot/BendyBot')
+.addField('Commands', 'the bendy bot commands')
+.addField('Can I be mod, Bendy?:', 'ask bendy if you can be mod :3')
+.addField('chapter three is coming out soon:', 'tell bendy that chapter three is coming soon')
+.addField('what is my avatar bendy?:', 'ask bendy what is your discord avatar')
+.addField('give me an inky rating:', 'ask bendy to rate you on a scale of 1-100')
+.addField('`when is chapter 3 coming:', 'ask bendy when chapter three is coming out')
+.addField('problems or suggestions?', 'if there are any problems or suggestions dm RICK SANCHEZ or Teller of fortune');
 bot.on('message', message => {
 if (message.content === prefix + 'help') {
-message.channel.sendMessage('`Can I be mod, Bendy?:` ask bendy if you can be mod :3')
-message.channel.sendMessage('`chapter three is coming out soon:` tell bendy that chapter three is coming soon')
-message.channel.sendMessage('`what is my avatar bendy?:` ask bendy what is your discord avatar')
-message.channel.sendMessage('`give me an inky rating:` ask bendy to rate you on a scale of 1-100')
-message.channel.sendMessage('`when is chapter 3 coming:` ask bendy when chapter three is coming out')
-message.channel.sendMessage('if there are any problems or suggestions dm <@271556689289805824> or <@313165926432505857>')
+message.channel.sendMessage({embed})
   }
 });
