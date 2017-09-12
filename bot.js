977968da328d54470cfbefe93add1fae3a377d1c
Saad Meliti <30853136+Saadmeliti@users.noreply.github.com>
2017-09-12 07:13:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'rip';
+const token = 'GOAWAYNOTOKENHERELOL';
 const prefix = "^"
 bot.on('ready', () => {
   console.log('BendyBot Ready');
@@ -47,22 +47,22 @@ message.channel.sendMessage('👹*This month...*👹');
 });
 const embed = new Discord.RichEmbed()
 .setTitle('BendyBot help')
-.setAuthor('BendyBot', 'https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
 .setColor(0x00AE86)
 .setDescription('all the commands for BendyBot')
 .setFooter('thats all the commands for now', 'https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
-.setThumbnail('https://cdn.discordapp.com/avatars/338246924278824970/638f5e3845bb7a166552b74c95c6cc08.png?size=2048')
 .setTimestamp()
 .setURL('https://github.com/BendyBot/BendyBot')
-.addField('Commands', 'the bendy bot commands')
-.addField('Can I be mod, Bendy?:', 'ask bendy if you can be mod :3')
-.addField('chapter three is coming out soon:', 'tell bendy that chapter three is coming soon')
-.addField('what is my avatar bendy?:', 'ask bendy what is your discord avatar')
-.addField('give me an inky rating:', 'ask bendy to rate you on a scale of 1-100')
-.addField('`when is chapter 3 coming:', 'ask bendy when chapter three is coming out')
+.addField('<:Bendy:335369477921439756> Commands <:Bendy:335369477921439756>', 'Can I be mod, Bendy?,  chapter three is coming out soon,  what is my avatar bendy?,  give me an inky rating,  give me an inky rating')
 .addField('problems or suggestions?', 'if there are any problems or suggestions dm RICK SANCHEZ or Teller of fortune');
 bot.on('message', message => {
 if (message.content === prefix + 'help') {
+message.react("335369477921439756")
 message.channel.sendMessage({embed})
+  }
+});
+bot.on('message', message => {
+if (message.content === '<@338246924278824970>') {
+message.react("337491354286292992")
+message.channel.sendMessage("what do you want? 😠")
   }
 });
